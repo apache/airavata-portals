edd8f1e53363075aeb9141ccfefc76d2df4c3241
yasithdev <yasith.j@icloud.com>
2026-04-11 13:20:06 -0500

    feat: remove Experiments and Datasets from top-level sidebar nav

--- airavata-django-portal/django_airavata/apps/workspace/apps.py
+++ airavata-django-portal/django_airavata/apps/workspace/apps.py
@@ -6,34 +6,22 @@ class WorkspaceConfig(AiravataAppConfig):
     label = "django_airavata_workspace"
     verbose_name = "Workspace"
     app_order = 0
-    url_home = "django_airavata_workspace:dashboard"
+    url_home = "django_airavata_workspace:projects"
     fa_icon_class = "fa-flask"
     app_description = """
         Launch applications and manage your experiments and projects.
     """
     nav = [
         {
-            "label": "Dashboard",
-            "icon": "fa fa-tachometer-alt",
-            "url": "django_airavata_workspace:dashboard",
-            "active_prefixes": ["applications", "dashboard"],
-        },
-        {
-            "label": "Experiments",
-            "icon": "fa fa-flask",
-            "url": "django_airavata_workspace:experiments",
-            "active_prefixes": ["experiments"],
+            "label": "Applications",
+            "icon": "fa fa-cubes",
+            "url": "django_airavata_workspace:applications",
+            "active_prefixes": ["applications"],
         },
         {
             "label": "Projects",
             "icon": "fa fa-box",
             "url": "django_airavata_workspace:projects",
             "active_prefixes": ["projects"],
-        },
-        {
-            "label": "Storage",
-            "icon": "fa fa-folder-open",
-            "url": "django_airavata_workspace:storage",
-            "active_prefixes": ["storage"],
         },
     ]
